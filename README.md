56dd195eebe2b0ab2ce60acc4a25b73f9aebd86d
Dani G <daniellegarcia@MacBook-Air-2.local>
2018-07-24 18:30:55 -0400

    homework submission

--- README.md
+++ README.md
@@ -5,9 +5,9 @@ Georgia Tech Bootcamp
 24 July 2018 
 
 ##Functionality
-Liri takes in 4 user commands including 
+Liri takes in 4 user commands including: 
 my-tweets
 spotify-this-song 'song title'
 movie-this 'movie title'
 do-what-it-says
-and spits out information about each. The first one retrievces that user's latest tweets. The second one uses the Spotify API and return data on the song the user inputs. The movie-this command grabs data from the OMDB API and returns specific data on the film entered by the user. The last command retrieves data from a file called random.txt and returns that text to the user. 
+and spits out different information about each. The first one retrievces the user's latest tweets and prints them to the terminal. The second one uses the Spotify API and returns data on the song the user inputs. The movie-this command grabs data from the OMDB API and returns specific data on the film entered by that user. The last command retrieves data from a file called random.txt and returns that which ever command or function is in that file. 
--- assets/js/liri.js
+++ assets/js/liri.js
@@ -13,77 +13,103 @@ var client = new Twitter(keys.twitter);
 console.log("Please enter one of the following commands:\n my-tweets \n spotify-this-song 'song title'\n movie-this 'movie title'\n do-what-it-says");
 
 var userInput = process.argv[2];
+var userInput1 = process.argv[3];
 var myTweets = "my-tweets";
 var spotifyInput = "spotify-this-song";
 var movieInput = "movie-this";
 var doWhatItSays = "do-what-it-says";
 
 // Tweets Output
-var params = {screen_name: 'danielleg16'};
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-  if (!error) {
-    for(var i = 0; i < tweets.length; i++) {
-        if(userInput == myTweets){
-        console.log(JSON.stringify(tweets[i].text, null, 2));
+function printTweets() {
+    var params = { screen_name: 'danielleg16' };
+    client.get('statuses/user_timeline', params, function (error, tweets, response) {
+        if (!error) {
+            for (var i = 0; i < tweets.length; i++) { 
+                    console.log(JSON.stringify(tweets[i].text, null, 2));
+            }
         }
-    }
-  }
 
-});
-
+    });
+};
 
 // Spotify Search 
-if(userInput == spotifyInput) {
-    var songTitle = process.argv[3];
+function spotifySearch(input1) {
+        songTitle = process.argv[3] || input1;
 
-spotify.search({ type: 'track', query: songTitle}, function(err, data) {
-    if (err) {
-      return console.log('Error occurred: ' + err);
-    }
-   
-    var artists = data.tracks.items[0].album.artists; 
-    var artistNames = [];
+        spotify.search({ type: 'track', query: songTitle }, function (err, data) {
+            if (err) {
+                return console.log('Error occurred: ' + err);
+            }
 
-    for (var i = 0; i < artists.length; i++) {
-        artistNames.push(artists[i].name);
-    }
+            var artists = data.tracks.items[0].album.artists;
+            var artistNames = [];
 
-    var songName = data.tracks.items[0].name;
-    var url = data.tracks.items[0].preview_url;
-    var albumName = data.tracks.items[0].album.name;
+            for (var i = 0; i < artists.length; i++) {
+                artistNames.push(artists[i].name);
+            }
 
-    console.log("Artist: " + artistNames);
-    console.log("Song Title: " + songName);
-    console.log("Link: " + url);
-    console.log("Album Name: " + albumName);
-  });
-}
+            var songName = data.tracks.items[0].name;
+            var url = data.tracks.items[0].preview_url;
+            var albumName = data.tracks.items[0].album.name;
+
+            console.log("Artist: " + artistNames);
+            console.log("Song Title: " + songName);
+            console.log("Link: " + url);
+            console.log("Album Name: " + albumName);
+        });
+};
 
 // OMDB Search
-if(userInput == movieInput) {
-    var movieTitle = process.argv[3];
-request("http://www.omdbapi.com/?t=" + movieTitle + "&y=&plot=short&apikey=trilogy&r=json", function(error, response, body) {
-  if (!error & response.statusCode === 200) {
-      var movie = JSON.parse(body);
-      console.log("Movie Title: " + movie.Title);
-      console.log("IMDB Rating: " + movie.imdbRating);
-      console.log("Rotten Tomatoes Rating: " + movie.Ratings[1].Value);
-      console.log("Country film was produced in: " + movie.Country);
-      console.log("Language: " + movie.Language);
-      console.log("Plot: " + movie.Plot);
-      console.log("Actors: " + movie.Actors);
-      
-  }
+function omdbSearch(input1) {
+        var movieTitle = process.argv[3] || input1;
+        request("http://www.omdbapi.com/?t=" + movieTitle + "&y=&plot=short&apikey=trilogy&r=json", function (error, response, body) {
+            if (!error & response.statusCode === 200) {
+                var movie = JSON.parse(body);
+                console.log("Movie Title: " + movie.Title);
+                console.log("IMDB Rating: " + movie.imdbRating);
+                console.log("Rotten Tomatoes Rating: " + movie.Ratings[1].Value);
+                console.log("Country film was produced in: " + movie.Country);
+                console.log("Language: " + movie.Language);
+                console.log("Plot: " + movie.Plot);
+                console.log("Actors: " + movie.Actors);
 
-});
-}
+            }
+
+        });
+};
+
 
 // Do what it says
-if(userInput == doWhatItSays){
-fs.readFile("random.txt", "utf8", function(error, data) {
-    if (error) {
-      return console.log(error);
-    } 
-    console.log(data.split(","));
-  });
+if (userInput == doWhatItSays) {
+    fs.readFile("random.txt", "utf8", function (err, data) {
+        if (err) {
+            output.error(err);
+        } else {
+            randomArray = data.split(",");
+            temp = randomArray[0];
+            input1 = randomArray[1];
+            spotifySearch(input1);
+            // omdbSearch(input1);
+            // printTweets();
+        }
+    });
+
+}
+
+function output(textOutput) {
+    log.info(textOutput);
+    console.log(textOutput);
+
+}
+
+
+if (userInput === myTweets) {
+    printTweets();
+}
+if (userInput === spotifyInput) {
+    spotifySearch();
+}
+
+if (userInput === movieInput) {
+    omdbSearch();
 }
